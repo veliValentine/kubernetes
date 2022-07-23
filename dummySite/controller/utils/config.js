a87382858e4b0993b5b8770e26bc4d581a7bcc95
veliValentine <nikkevanukas1995@gmail.com>
2022-07-23 15:27:32 +0300

    feat: env

--- dummySite/controller/utils/config.js
+++ dummySite/controller/utils/config.js
@@ -1,9 +1,14 @@
 import 'dotenv/config'
 
+export const PROD = 'prod'
+export const DEV = 'dev'
+
 const {
-  PORT = 3000
+  PORT = 3000,
+  NODE_ENV = PROD
 } = process.env
 
 export default {
-  PORT
+  PORT,
+  NODE_ENV
 }
