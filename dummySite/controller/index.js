d03162ef607bab8d5725f45b71719db9cfed4f7f
veliValentine <nikkevanukas1995@gmail.com>
2022-07-23 15:27:19 +0300

    feat: kube api

--- dummySite/controller/index.js
+++ dummySite/controller/index.js
@@ -1,29 +1,72 @@
 import axios from 'axios'
-
 import http from 'http'
-
 import config from './utils/config.js'
 import logger from './utils/logger.js'
 
-const getSite = async (url = 'https://example.com/') => {
+import k8s from '@kubernetes/client-node'
+
+const getErrorMessage = error => error?.response?.data ?? error?.message ?? error
+
+const kc = new k8s.KubeConfig()
+kc.loadFromCluster()
+
+const dummySitesApiUrl = '/apis/stable.dwk/v1/dummysites'
+
+const sendRequestToApi = async (api, method = 'get') => {
+  try {
+    const urlHost = kc.getCurrentCluster().server
+    const url = `${urlHost}${api}`
+
+    logger.log('Sending request to api', JSON.stringify({ method, url }))
+    const { data } = await axios[method](url)
+    return data
+  } catch (error) {
+    const errorMessage = getErrorMessage(error)
+    logger.error('Error sending request to api.', JSON.stringify({ api, method }), 'Error message:', errorMessage)
+  }
+}
+
+
+const getDummySiteInfo = async () => {
+  const dummySites = await sendRequestToApi(dummySitesApiUrl)
+  console.log({ dummySites })
+
+  if (!dummySites?.length) {
+    throw new Error('no dummy sites')
+  }
+  const url = dummySites[0].spec.website_url
+  return { url }
+}
+
+const getSite = async (url) => {
   try {
     const { data } = await axios.get(url)
     return data
   } catch (error) {
-    const errorMessage = error?.response?.data ?? error?.message ?? error
-    logger.error(errorMessage)
+    const errorMessage = getErrorMessage(error)
+    logger.error('Error fetching site:', errorMessage)
   }
 }
 
-const createSite = (html = '<h1>Nothing to show!</h1>') => {
-  http.createServer((_req, res) => {
+const createServer = (html = '<h1>Nothing to show!</h1>') => {
+  const requestListener = (_req, res) => {
     res.writeHead(200, {
       'Content-Type': 'text/html'
     })
     res.write(html)
     res.end()
-  }).listen(config.PORT)
-  logger.log(`Server running on port: ${config.PORT}`)
+  }
+  const server = http.createServer(requestListener)
+
+  const port = config.PORT
+  server.listen(port)
+  logger.log(`Server running on port: ${port}`)
 }
 
-createSite()
+const app = async () => {
+  const { url } = await getDummySiteInfo()
+  const site = await getSite(url)
+  createServer(site)
+}
+
+app()
